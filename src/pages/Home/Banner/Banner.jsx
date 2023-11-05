cae53d37a1f131540bf1752bf924545ac5eadf99
chayansrk64 <chayansrk64@gmail.com>
2023-11-05 20:21:00 +0600

    initial commit

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className="text-3xl font-bold underline">
+            <h2>This is banner section</h2>
+             
+        </div>
+    );
+};
+
+export default Banner;
